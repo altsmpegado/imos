a9fcf2648592817de91c2715e4c7b65cbf9e43b5
alts <altsmpegado@gmail.com>
2024-01-05 15:19:58 +0000

    camera-service http web mount

--- imos-app/docker-app/src/renderer.js
+++ imos-app/docker-app/src/renderer.js
@@ -9,11 +9,11 @@ navigator.mediaDevices.getUserMedia({ video: true })
     console.error('Error accessing camera:', error.message);
   });
 
-  navigator.mediaDevices.enumerateDevices()
-  .then(devices => {
-    console.log('Available devices:', devices);
-    // Your logic to handle the list of available devices
-  })
-  .catch(error => {
-    console.error('Error enumerating devices:', error.message);
-  });
+navigator.mediaDevices.enumerateDevices()
+.then(devices => {
+  console.log('Available devices:', devices);
+  // Your logic to handle the list of available devices
+})
+.catch(error => {
+  console.error('Error enumerating devices:', error.message);
+});
