42e514668aa65bd8993c0aa99d6e6c6d0fc0fbbf
alts <altsmpegado@gmail.com>
2024-02-13 19:34:19 +0000

    new react app for minikube

---
+++ app-example-local/docker-app-local/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [cameraFeedError, setCameraFeedError] = useState(false);
+
+  useEffect(() => {
+    const img = document.createElement('img');
+    img.src = 'http://192.168.1.68:5000/video_feed';
+
+    img.onload = () => {
+      document.body.appendChild(img);
+    };
+
+    img.onerror = (error) => {
+      console.error('Error loading camera feed:', error);
+      setCameraFeedError(true);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      {cameraFeedError && <p id="info">Failed to load camera feed.</p>}
+    </div>
+  );
+}
+
+export default App;
