b035208a59083658155ec01fa9a4e48732349267
alts <altsmpegado@gmail.com>
2024-02-21 21:34:40 +0000

    marketplace frontend dev

--- imos-app/marketplace-app/renderer.js
+++ imos-app/marketplace-app/renderer.js
@@ -2,10 +2,11 @@ const { ipcRenderer } = require('electron');
 const fs = require('fs');
 
 function clickApp(){
-  const appcard = document.querySelectorAll(".appcard");
-  appcard.forEach((item)=>{
-    const data = item.dataset.app;
-    item.addEventListener('click', ()=>{
+  const appcard = document.querySelectorAll(".product");
+  appcard.forEach((item)=>{ 
+    item.addEventListener('click', (event) =>{
+      event.preventDefault();
+      const data = item.dataset.app;
       console.log(data);
       ipcRenderer.send('openAppWindow', data)
     })
@@ -19,44 +20,28 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('http://localhost:8000/apps')
     .then((response) => response.json())
     .then((apps) => {
-      const appListDiv = document.getElementById('appList');
+      const appListDiv = document.getElementById('apps-container');
       
-      // Create download button for each app
       apps.forEach((app) => {
         console.log(app);
         const cardHtml = `
-        <div class="col-xl-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
-            <div class="themes-box">
-                <div class="thumb">
-                    <img src="assets/images/unique-theme/unique-2.png" alt="">
-                    <div class="button-box">
-                        <a href="#0" class="appcard link link-1" data-app="${JSON.stringify(app).replace(/"/g, '&quot;')}"><i class="far fa-eye"></i></a>
-                        <!--a href="#0" class="link link-2"><i class="fas fa-cart-plus"></i></a-->
-                        <!--a href="#0" class="link link-3"><i class="fas fa-heart"></i></a-->
-                    </div>
+        <div class="product-card">
+          <a href="0#" class="product" data-app="${JSON.stringify(app).replace(/"/g, '&quot;')}">
+            <img class="app-icon" src="C:\\imos\\imos-app\\marketplace-app\\views\\apps.8985.13655054093851568.1c669dab-3716-40f6-9b59-de7483397c3a.png"></img>
+            <div class="info-container">
+              <p class="title">${app.name}</p>
+              <div class="subtitle">
+                <div class="rating-element">
+                  <span class="label">4.2</span>
+                  <span class="label">&#9733;</span>
                 </div>
-                <div class="main-content">
-                    <h5><a class="appcard" href="#0" data-app="${JSON.stringify(app).replace(/"/g, '&quot;')}">${app.name}</a></h5>
-                    <div class="last-part">
-                        <div class="right">
-                            <p class="text">
-                                by <a href="#0" class="link">${app.company}</a> in <a href="#0" class="link">Agency</a>
-                            </p>
-                            <div class="review">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star-half-alt"></i>
-                                <i class="far fa-star"></i>
-                            </div>
-                        </div>
-                        <div class="left-content">
-                            <h3 class="subtitle">59€</h3>
-                            <span>1247 Downloads</span>
-                        </div>
-                    </div>
-                </div>
+                <div class="text-ellipsis">| ${app.company}</div>
+              </div>
             </div>
+            <div class="price-container">
+              <button class="more-button">More</button>
+            </div>
+          </a>
         </div>
         `
         appListDiv.innerHTML += cardHtml;
@@ -67,24 +52,34 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error('Error fetching app information:', error);
     });
 
-  if (type == 'developer'){
-    const moreFuncDiv = document.getElementById('moreFunc');
-    const devButton = document.createElement('button');
-    devButton.textContent = 'Submit App';
-    devButton.classList.add('main-btn'); 
-    devButton.addEventListener('click', () => {
-      ipcRenderer.send('openDevForm');
-    });
-    moreFuncDiv.appendChild(devButton);
-    
+    if (type == 'developer') {
+      const moreFuncDiv = document.getElementById('btn-top');
+      const devButton = document.createElement('button');
+      devButton.classList.add('sidebtn');
+  
+      // Create the icon element and add it to the button
+      const icon = document.createElement('i');
+      icon.classList.add('material-symbols-outlined');
+      icon.textContent = 'new_window'; // Set the icon's text content to the desired material icon identifier
+  
+      devButton.appendChild(icon); // Add the icon to the button
+  
+      devButton.addEventListener('click', () => {
+        ipcRenderer.send('openDevForm');
+      });
+  
+      moreFuncDiv.appendChild(devButton);
+    /*
     const subStateButton = document.createElement('button');
     subStateButton.textContent = 'Submissions';
-    subStateButton.classList.add('main-btn'); 
+    subStateButton.classList.add('btn-bot'); 
     subStateButton.addEventListener('click', () => {
       ipcRenderer.send('openSubmissions');
     });
     
     moreFuncDiv.appendChild(subStateButton);
+    */
   }
+
   
 });
