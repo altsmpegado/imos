7e4439ea0cbbadf39bc63f7c7379a0c55fa6ed2b
alts <altsmpegado@gmail.com>
2024-02-25 17:34:35 +0000

    app page frontend

--- imos-app/marketplace-app/app_renderer.js
+++ imos-app/marketplace-app/app_renderer.js
@@ -47,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 <div class="reviews">
                     <h4 class="tab-title">Reviews</h4>
+                    <h1 style="margin-left: 25px; font-size:3.0rem;">4.3</h1>
                     <p class="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
                 </div>
                 <div class="additional">
