d59b324acbbff2eba2385d961771eae389dbf8bd
alts <altsmpegado@gmail.com>
2024-05-01 10:50:34 +0100

    roboflow app dockerize

--- apps/roboflowapp/imos-roboflowapp/app.py
+++ apps/roboflowapp/imos-roboflowapp/app.py
@@ -1,21 +1,33 @@
-from flask import Flask, request, jsonify
-from inference_sdk import InferenceHTTPClient
-from PIL import Image
+import os
 import supervision as sv
 import uuid
-import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS # type: ignore
+from inference_sdk import InferenceHTTPClient, InferenceConfiguration
+from PIL import Image
 
 app = Flask(__name__)
+CORS(app)
+
+port = os.getenv('PORT', '5002:5002').split(':')[1]
+api_url = os.getenv('ROBOFLOW_API_URL', 'https://detect.roboflow.com')
+api_key = os.getenv('ROBOFLOW_API_KEY', '4BRQaWP7CIwFCAqFJkHW')
+project_id = os.getenv('ROBOFLOW_PROJECT_ID', 'ppe-dataset-for-workplace-safety')
+model_version = os.getenv('MODEL_VERSION', '1')
+confidence_threshold = os.getenv('CONFIDENCE', '0.5')
 
 # Set up the client for inference
 client = InferenceHTTPClient(
-    api_url="https://detect.roboflow.com",
-    api_key="4BRQaWP7CIwFCAqFJkHW",
+    api_url=api_url,
+    api_key=api_key,
 )
+custom_configuration = InferenceConfiguration(confidence_threshold=0.8)
 
-# Define the project ID and model version
-project_id = "ppe-dataset-for-workplace-safety"
-model_version = 1
+UPLOAD_FOLDER = '/uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/annotate', methods=['POST'])
 def annotate_image():
@@ -26,11 +38,16 @@ def annotate_image():
     # Get the uploaded image file
     file = request.files['file']
     
+    filename = str(uuid.uuid4()) + '.jpg'
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
     # Load the image using PIL
-    image = Image.open(file)
+    image = Image.open(filepath)
 
     # Run inference on the image
-    results = client.infer(image, model_id=f"{project_id}/{model_version}")
+    with client.use_configuration(custom_configuration):
+        results = client.infer(image, model_id=f"{project_id}/{model_version}")
     
     # Get detections from the inference results
     detections = sv.Detections.from_inference(results)
@@ -57,4 +74,4 @@ def annotate_image():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
