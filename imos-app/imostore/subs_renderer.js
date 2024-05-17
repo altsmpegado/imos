17682e171ed7be53691baf65ad74a0be1da7c242
alts <altsmpegado@gmail.com>
2024-05-17 17:08:43 +0100

    submissions history state frontend

--- imos-app/imostore/subs_renderer.js
+++ imos-app/imostore/subs_renderer.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   ipcRenderer.on('subsInfo', async (event, user) => {
     try {
-      const subInfoDiv = document.getElementById('subInfo');
+      const subInfoDiv = document.getElementById('subs-container');
       const data = await fetch(`http://localhost:8000/subs/${user}`).then(response => response.json());
       const subAppsData = await Promise.all(data.subApps.map(async (id) => {
         const subApp = await fetch(`http://localhost:8000/sub/${id}`).then(response => response.json());
@@ -12,16 +12,23 @@ document.addEventListener('DOMContentLoaded', async () => {
       }));
 
       subAppsData.forEach((subApp) => {
-        const subInfo = document.createElement('div');
-        subInfo.innerHTML = `
-          <h2>${subApp.appname}</h2>
-          <p>Provider: ${subApp.company}</p>
-          <p>Version: ${subApp.version}</p>
-          <p>State: ${subApp.state}</p>
-        `;
-        subInfoDiv.appendChild(subInfo);
-        const line = document.createElement('hr');
-        subInfo.appendChild(line);
+        const stateText = subApp.state ? 'Accepted' : 'Under Review';
+        const stateColor = subApp.state ? 'green' : 'orange';
+        const cardHtml = `
+          <div class="product-card">
+            <div class="product">
+              <div style="display: flex; align-items: center; gap: 1rem;">
+                  <p class="title">${subApp.appname}</p>
+                  <div class="subtitle">
+                    <p style:"margin-bottom:10px;>Provider: ${subApp.company}</p><br>
+                    <p>Version: ${subApp.version}</p><br>
+                    <p style="color: ${stateColor};">State: ${stateText}</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        `
+        subInfoDiv.innerHTML += cardHtml;
       });
     } catch (error) {
       console.error('Error fetching user submitted apps:', error);
